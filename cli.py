974a49ceef036e48d8cdb0d46b3526b35361b71e
NaimaHasan <naimahasan31@gmail.com>
2024-06-04 12:59:18 +0600

    fix: reorganize functions in cli.py and service.py

--- cli.py
+++ cli.py
@@ -1,9 +1,5 @@
 import typer
-import os
-from rich.console import Console
-from rich.table import Table
-
-from services.service import load_recipes, find_recipes_by_ingredients
+import services.service as service
 
 app = typer.Typer(no_args_is_help=True)
 
@@ -15,21 +11,30 @@ ingredients = [
     "Sugar"
 ]
 
-storage_file = "added_ingredients.txt"
+added_ingredients = service.load_added_ingredients()
 
 
-def bold(text: str) -> str:
-    return f"\033[1m{text}\033[0m"
+@app.command()
+def find_all_recipes():
+    """List all recipes"""
+
+    recipes = service.load_recipes()
+    typer.echo("All Recipes:")
+    service.display_recipe_list(recipes)
 
 
-def load_added_ingredients():
-    if os.path.exists(storage_file):
-        with open(storage_file, "r") as file:
-            return {line.strip() for line in file.readlines()}
-    return set()
+@app.command()
+def find_recipe_by_id(recipe_id: int):
+    """Find a specific recipe by ID"""
 
+    recipes = service.load_recipes()
 
-added_ingredients = load_added_ingredients()
+    recipe = next((recipe for recipe in recipes if recipe.id == recipe_id), None)
+
+    if recipe is not None:
+        service.display_recipe(recipe)
+    else:
+        typer.echo(f"Recipe with ID {recipe_id} not found.")
 
 
 @app.command()
@@ -39,14 +44,13 @@ def add_ingredients(espresso: bool = typer.Option(False, "--espresso", help="Add
                     cream: bool = typer.Option(False, "--cream", help="Add Cream"),
                     sugar: bool = typer.Option(False, "--sugar", help="Add Sugar")):
     """Add ingredients"""
-    options = ["espresso", "milk", "caramel", "cream", "sugar"]
 
-    for option in options:
-        if vars()[option]:
-            added_ingredients.add(option.capitalize())
+    for option in ingredients:
+        if vars()[option.lower()]:
+            added_ingredients.add(option)
 
-    save_added_ingredients(added_ingredients)
-    show_ingredients()
+    service.save_added_ingredients(added_ingredients)
+    service.display_ingredients(ingredients, added_ingredients)
 
 
 @app.command()
@@ -56,100 +60,39 @@ def remove_ingredients(espresso: bool = typer.Option(False, "--espresso", help="
                        cream: bool = typer.Option(False, "--cream", help="Remove Cream"),
                        sugar: bool = typer.Option(False, "--sugar", help="Remove Sugar")):
     """Remove added ingredients"""
-    options = ["espresso", "milk", "caramel", "cream", "sugar"]
 
-    for option in options:
-        if vars()[option]:
-            if option.capitalize() in added_ingredients:
-                added_ingredients.remove(option.capitalize())
-                typer.echo(f"Removed ingredient: {option.capitalize()}")
+    for option in ingredients:
+        if vars()[option.lower()]:
+            if option in added_ingredients:
+                added_ingredients.remove(option)
+                typer.echo(f"Removed ingredient: {option}")
             else:
-                typer.echo(f"{option.capitalize()} is not in the added ingredients.")
+                typer.echo(f"{option} is not in the added ingredients.")
 
-    save_added_ingredients(added_ingredients)
-    show_ingredients()
+    service.save_added_ingredients(added_ingredients)
+    service.display_ingredients(ingredients, added_ingredients)
 
 
 @app.command()
 def clear_ingredients():
     """Clear added ingredients"""
+
     added_ingredients.clear()
-    save_added_ingredients(added_ingredients)
+    service.save_added_ingredients(added_ingredients)
     typer.echo("Added ingredients cleared.")
-
-
-def save_added_ingredients(added_ingredients):
-    with open(storage_file, "w") as file:
-        for ingredient in added_ingredients:
-            file.write(f"{ingredient}\n")
-
-
-def show_ingredients():
-    print(f"Available Ingredients:")
-    for index, ingredient in enumerate(ingredients, start=1):
-        print(f"{index}. {ingredient}")
-
-    if added_ingredients:
-        print("\nAdded Ingredients:")
-        for index, ingredient in enumerate(added_ingredients, start=1):
-            print(f"{index}. {ingredient}")
-    else:
-        print("\nNo ingredients added.")
 
 
 @app.command()
 def find_recipe_by_added_ingredients():
     """Find recipe by added ingredients"""
-    recipes = load_recipes()
-    searched_recipes = find_recipes_by_ingredients(recipes, added_ingredients)
-    display_recipes(searched_recipes)
 
+    recipes = service.load_recipes()
+    searched_recipes = service.find_recipes_by_ingredients(recipes, added_ingredients)
 
-@app.command()
-def find_all_recipes():
-    """List all recipes"""
-    recipes = load_recipes()
-    typer.echo("All Recipes:")
-    display_recipes(recipes)
-
-
-def display_recipes(recipes):
-    console = Console()
-    table = Table("ID", "Name")
-
-    for recipe in recipes:
-        table.add_row(str(recipe.id), recipe.name)
-
-    console.print(table)
-
-
-@app.command()
-def find_recipe_by_id(recipe_id: int):
-    """Find a specific recipe by ID"""
-    recipes = load_recipes()
-
-    for recipe in recipes:
-        if recipe.id == recipe_id:
-            display_recipe(recipe)
-
-
-def display_recipe(recipe):
-    console = Console()
-
-    table = Table("Name", "Description")
-
-    table.add_row("Name", recipe.name)
-    table.add_row("Category", recipe.category)
-    table.add_row("Description", recipe.description)
-
-    ingredients_str = "\n".join(
-        [f"- {ingredient['name']}: {ingredient['quantity']}" for ingredient in recipe.ingredients])
-    table.add_row("Ingredients", ingredients_str)
-
-    steps_str = "\n".join([f"{step['number']}. {step['description']}" for step in recipe.steps])
-    table.add_row("Steps", steps_str)
-
-    console.print(table)
+    if not searched_recipes:
+        print("No recipes found.")
+    else:
+        service.display_recipe_list(searched_recipes)
 
 
 if __name__ == "__main__":
--- services/service.py
+++ services/service.py
@@ -1,7 +1,24 @@
 import requests
 from models.recipe import Recipe
+from rich.console import Console
+from rich.table import Table
+import os
 
 recipe_url = "http://naima-hasan-espresso-json.vercel.app/db"
+storage_file = "added_ingredients.txt"
+
+
+def load_added_ingredients():
+    if os.path.exists(storage_file):
+        with open(storage_file, "r") as file:
+            return {line.strip() for line in file.readlines()}
+    return set()
+
+
+def save_added_ingredients(added_ingredients):
+    with open(storage_file, "w") as file:
+        for ingredient in added_ingredients:
+            file.write(f"{ingredient}\n")
 
 
 def load_recipes():
@@ -30,3 +47,45 @@ def create_recipe_from_data(data):
 def find_recipes_by_ingredients(recipes, ingredient_names):
     ingredient_names = {name.lower() for name in ingredient_names}
     return [recipe for recipe in recipes if recipe.has_ingredients(ingredient_names)]
+
+
+def display_recipe(recipe):
+    console = Console()
+
+    table = Table("Name", "Description")
+
+    table.add_row("Name", recipe.name)
+    table.add_row("Category", recipe.category)
+    table.add_row("Description", recipe.description)
+
+    ingredients_str = "\n".join(
+        [f"- {ingredient['name']}: {ingredient['quantity']}" for ingredient in recipe.ingredients])
+    table.add_row("Ingredients", ingredients_str)
+
+    steps_str = "\n".join([f"{step['number']}. {step['description']}" for step in recipe.steps])
+    table.add_row("Steps", steps_str)
+
+    console.print(table)
+
+
+def display_recipe_list(recipes):
+    console = Console()
+    table = Table("ID", "Name")
+
+    for recipe in recipes:
+        table.add_row(str(recipe.id), recipe.name)
+
+    console.print(table)
+
+
+def display_ingredients(ingredients, added_ingredients):
+    print(f"Available Ingredients:")
+    for index, ingredient in enumerate(ingredients, start=1):
+        print(f"{index}. {ingredient}")
+
+    if added_ingredients:
+        print("\nAdded Ingredients:")
+        for index, ingredient in enumerate(added_ingredients, start=1):
+            print(f"{index}. {ingredient}")
+    else:
+        print("\nNo ingredients added.")
